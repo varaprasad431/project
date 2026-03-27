a02b99da167bde36a30897e05eda1ee7da418d0c
varaprasad431 <147475432+varaprasad431@users.noreply.github.com>
2026-03-27 12:05:59 +0530

    Update test_operation.py

--- test/test_operation.py
+++ test/test_operation.py
@@ -1,6 +1,8 @@
-from src.math_op import add,sub
+from src.math_op import add, sub
+
 def test_add():
-  assert (2,3)==5
-  assert (-1,1)==0
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
 def test_sub():
-  assert (5,3)==2
+    assert sub(5, 3) == 2
